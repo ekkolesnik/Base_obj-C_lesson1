45ca5f07caf97a8e61878dff05329367846d6154
ekkolesnik <grid.box@gmail.com>
2020-07-18 21:27:10 +0300

    add project

---
+++ Lesson1/Lesson1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Lesson1
+//
+//  Created by Evgeny Kolesnik on 18.07.2020.
+//  Copyright © 2020 Evgeny Kolesnik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
