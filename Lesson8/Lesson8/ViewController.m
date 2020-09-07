0ae4e572eac1d117b95ee1f8a256bbc5aee0fe13
ekkolesnik <grid.box@gmail.com>
2020-09-07 07:47:11 +0300

    lesson8 done

--- Lesson8/Lesson8/ViewController.m
+++ Lesson8/Lesson8/ViewController.m
@@ -16,8 +16,34 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    NSLog(@"Controller is loaded.");
+    
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    NSLog(@"Controller is ready to show view.");
+}
+
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    NSLog(@"Controller is showed now.");
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    NSLog(@"Controller is ready to hide view.");
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    NSLog(@"Controller is hidden now.");
+}
+
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
+    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
+    NSLog(@"Controller is rotated.");
+}
 
 @end
